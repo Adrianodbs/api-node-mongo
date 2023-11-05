dccd664666e18b86c4c95763c014143dd638a43b
Adriano <adriano_alves_2008@hotmail.com>
2023-11-05 20:54:45 -0300

    feat: create frontend style

---
+++ frontend/src/App.tsx
@@ -0,0 +1,53 @@
+import { FiTrash } from 'react-icons/fi'
+
+function App() {
+  return (
+    <div className="w-full min-h-screen bg-gray-900 flex justify-center px-4">
+      <main className="my-10 w-full md:max-w-2xl">
+        <h1 className="text-4xl font-medium text-white">Clientes</h1>
+
+        <form className="flex flex-col my-6">
+          <label className="font-medium text-white">Nome:</label>
+          <input
+            type="text"
+            placeholder="Digite o seu nome"
+            className="w-full mb-5 p-2 rounded"
+          />
+
+          <label className="font-medium text-white">Email:</label>
+          <input
+            type="email"
+            placeholder="Digite o seu e-mail"
+            className="w-full mb-5 p-2 rounded"
+          />
+
+          <input
+            type="submit"
+            value="Cadastrar"
+            className="cursor-pointer w-full p-2 bg-green-500 rounded font-medium"
+          />
+        </form>
+
+        <section className="flex flex-col">
+          <article className="w-full bg-white rounded p-2 relative hover:scale-105 duration-200">
+            <p>
+              <span className="font-medium">Nome:</span> Adriano
+            </p>
+            <p>
+              <span className="font-medium">Email:</span> Adriano@teste.com
+            </p>
+            <p>
+              <span className="font-medium">Status:</span> Ativo
+            </p>
+
+            <button className="bg-red-500 w-7 h-7 flex items-center justify-center rounded-lg absolute right-0 -top-2">
+              <FiTrash size={18} color="#fff" />
+            </button>
+          </article>
+        </section>
+      </main>
+    </div>
+  )
+}
+
+export default App
